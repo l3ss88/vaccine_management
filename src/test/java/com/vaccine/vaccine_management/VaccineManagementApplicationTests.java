5ef5660708251c845f627eeda5d1e9c0b511b40d
Lesly <les560@hotmail.com>
2024-11-06 20:28:00 -0500

    First commit

---
+++ src/test/java/com/vaccine/vaccine_management/VaccineManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vaccine.vaccine_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaccineManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
