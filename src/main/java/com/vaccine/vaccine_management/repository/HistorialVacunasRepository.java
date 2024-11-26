05f2c07936d8abd9aaa695bde648eb787fb7f1bf
kevinCmpFlores <kevincfj29@gmail.com>
2024-11-25 20:48:15 -0500

    commit - historialvacunasv4

--- src/main/java/com/vaccine/vaccine_management/repository/HistorialVacunasRepository.java
+++ src/main/java/com/vaccine/vaccine_management/repository/HistorialVacunasRepository.java
@@ -1,9 +1,13 @@
 package com.vaccine.vaccine_management.repository;
 
 import com.vaccine.vaccine_management.model.HistorialVacunas;
-import org.hibernate.mapping.List;
+
+import com.vaccine.vaccine_management.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
 
 @Repository
 public interface HistorialVacunasRepository extends JpaRepository<HistorialVacunas, Long > {
