bb6edeaf02e3975e9b14e608093991df3389800c
kevinCmpFlores <kevincfj29@gmail.com>
2024-11-25 20:37:29 -0500

    commit - historialvacunas

--- src/main/java/com/vaccine/vaccine_management/service/HistorialVacunasService.java
+++ src/main/java/com/vaccine/vaccine_management/service/HistorialVacunasService.java
@@ -1,10 +1,15 @@
 package com.vaccine.vaccine_management.service;
 
+
+
 import com.vaccine.vaccine_management.model.HistorialVacunas;
+import com.vaccine.vaccine_management.model.Usuario;
 import com.vaccine.vaccine_management.repository.HistorialVacunasRepository;
-import org.hibernate.mapping.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+
 
 @Service
 public class HistorialVacunasService {
